008eeaba5e4439d2d1aab9b3cbae8659f1f0948e
SerovaK4 <ksusha1354@gmail.com>
2023-03-13 15:26:42 +0300

    add docstring for function 2

--- str_func.py
+++ str_func.py
@@ -1,3 +1,7 @@
-#функция возвращает все слова в верхнем регистре
 def str_func_upper(user_string):
     return user_string.upper()
+
+
+#Функция делает заглавными первые буквы каждого слова в строке
+def str_func_title(user_string):
+    return user_string.title()
